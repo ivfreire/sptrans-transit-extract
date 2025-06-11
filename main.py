d15e37f1f5b47dc37310deaa758cd01bacc17fa9
Ícaro Freire <icaro.freire@tempook.com>
2025-06-10 21:44:41 -0300

    :tada: first files

---
+++ main.py
@@ -0,0 +1,82 @@
+#   ===================================
+#   Extracts data from Olho Aberto API.
+#   ===================================
+
+import requests
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+import pandas_gbq as gbq
+
+BASE_URL = 'https://olhovivo.sptrans.com.br/'
+EXPORT_COLUMNS = ['line_display', 'line_code', 'line_direction', 'bus_prefix', 'is_accessible', 'timestamp', 'lat', 'lon']
+
+def create_session() -> requests.Session:
+    """
+    Creates a new session for making API requests.
+    Returns:
+        A requests.Session object.
+    """
+    
+    session = requests.Session()
+    session.get(BASE_URL)
+    
+    return session
+
+def _request(session) -> pd.DataFrame:
+    request = session.get(url=f'{BASE_URL}/data/Posicao')
+
+    if request.status_code != 200:
+        raise Exception(f"Failed to fetch data: {request.status_code}, {request.text}")
+    
+    data = request.json()
+
+    return pd.DataFrame(data['l'])
+
+def format(info_df: pd.DataFrame, export_columns: list) -> pd.DataFrame:
+    info_df.columns = ['line_display', 'line_code', 'line_direction', 'to', 'from', 'bus_count', 'buses']
+
+    buses_df = info_df.explode('buses').reset_index(drop=True)
+
+    bus_info_df = pd.json_normalize(buses_df['buses'])
+    bus_info_df.columns = ['bus_prefix', 'b', 'is_accessible', 'timestamp', 'lat', 'lon', 'sv', 'is']
+
+    bus_info_df = pd.concat([
+        buses_df.drop(columns=['buses']),
+        bus_info_df
+    ], axis=1)
+
+    bus_info_df['timestamp'] = pd.to_datetime(bus_info_df['timestamp'])
+    bus_info_df['ingestion_time'] = datetime.utcnow()
+
+    return bus_info_df[['ingestion_time'] + export_columns]
+
+def upload(info_df: pd.DataFrame) -> None:
+    gbq.to_gbq(
+        info_df,
+        destination_table='sptransit.transit.bus_position',
+        project_id='sptransit',
+        if_exists='append'
+    )
+
+def main(request):
+    """
+    GCP Cloud Function entry point.
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        A response object.
+    """
+
+    session = create_session()
+
+    info_df = _request(session)
+    info_df = format(info_df, EXPORT_COLUMNS)
+
+    upload(info_df)
+
+    return "Hello from Olho Aberto API extractor!", 200
+
+if __name__ == '__main__':
+    main(None)
